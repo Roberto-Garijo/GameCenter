f406f779776b14df63e25c41f4dc62a312bf7565
pauprats2002 <77688104+pauprats2002@users.noreply.github.com>
2022-01-12 20:54:45 +0100

    Fondo insertado
    Fondo insertado, solucionar errores.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,44 +5,54 @@ import 'package:game_center/pages/page3.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 
+// ignore: use_key_in_widget_constructors
 class HomePage extends StatelessWidget {
   final _controller = PageController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          //page view
-          SizedBox(
-            height: 500,
-            child: PageView(
-              controller: _controller,
-              children: const [
-                  Page1(),
-                  Page2(),
-                  Page3()
-              ],
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/Rocket.jpg"),
+        fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            //page view
+            SizedBox(
+              height: 500,
+              child: PageView(
+                controller: _controller,
+                children: const [
+                    Page1(),
+                    Page2(),
+                    Page3()
+                ],
+              ),
             ),
-          ),
 
-          //dot
-          SmoothPageIndicator(
-            controller: _controller, 
-            count: 3,
-            effect: JumpingDotEffect(
-              activeDotColor: Colors.blueAccent,
-              dotColor: Colors.blue.shade100,
-              dotHeight: 23,
-              dotWidth: 23,
-              spacing: 16,
-              verticalOffset: 20
+            //dot
+            SmoothPageIndicator(
+              controller: _controller, 
+              count: 3,
+              effect: JumpingDotEffect(
+                activeDotColor: Colors.blueAccent,
+                dotColor: Colors.blue.shade100,
+                dotHeight: 23,
+                dotWidth: 23,
+                spacing: 16,
+                verticalOffset: 20
+              ),
+              onDotClicked:(index) {
+              } ,
             ),
-            onDotClicked:(index) {
-            } ,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
