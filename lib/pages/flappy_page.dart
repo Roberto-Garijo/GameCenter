ee96c3d9557405dd0be3683d8e8245c31412f4bc
Roberto-Garijo <robertogarijo@paucasesnovescifp.cat>
2022-01-17 15:23:19 +0100

    Updated flappy bird page

--- lib/pages/flappy_page.dart
+++ lib/pages/flappy_page.dart
@@ -13,25 +13,20 @@ class FlappyBirdPage extends StatefulWidget {
 }
 
 class _FlappyBirdPage extends State<FlappyBirdPage> {
-  // bird variables
   static double birdY = 0;
   double initialPos = birdY;
   double height = 0;
   double time = 0;
-  double gravity = -4.9; // how strong the gravity is
-  double velocity = 3.5; // how strong the jump is
-  double birdWidth = 0.1; // out of 2, 2 being the entire width of the screen
-  double birdHeight = 0.1; // out of 2, 2 being the entire height of the screen
+  double gravity = -4.9;
+  double velocity = 3.5;
+  double birdWidth = 0.1;
+  double birdHeight = 0.1;
 
-  // game settings
   bool gameHasStarted = false;
 
-  // barrier variables
   static List<double> barrierX = [2, 2 + 1.5];
-  static double barrierWidth = 0.5; // out of 2
+  static double barrierWidth = 0.5;
   List<List<double>> barrierHeight = [
-    // out of 2, where 2 is the entire height of the screen
-    // [topHeight, bottomHeight]
     [0.6, 0.4],
     [0.4, 0.6],
   ];
@@ -39,36 +34,26 @@ class _FlappyBirdPage extends State<FlappyBirdPage> {
   void startGame() {
     gameHasStarted = true;
     Timer.periodic(Duration(milliseconds: 10), (timer) {
-      // a real physical jump is the same as an upside down parabola
-      // so this is a simple quadratic equation
       height = gravity * time * time + velocity * time;
 
       setState(() {
         birdY = initialPos - height;
       });
-
-      // check if bird is dead
       if (birdIsDead()) {
         timer.cancel();
         _showDialog();
       }
-
-      // keep the map moving (move barriers)
       moveMap();
-
-      // keep the time going!
       time += 0.01;
     });
   }
 
   void moveMap() {
     for (int i = 0; i < barrierX.length; i++) {
-      // keep barriers moving
       setState(() {
         barrierX[i] -= 0.005;
       });
 
-      // if barrier exits the left part of the screen, keep it looping
       if (barrierX[i] < -1.5) {
         barrierX[i] += 3;
       }
@@ -76,7 +61,7 @@ class _FlappyBirdPage extends State<FlappyBirdPage> {
   }
 
   void resetGame() {
-    Navigator.pop(context); // dismisses the alert dialog
+    Navigator.pop(context);
     setState(() {
       birdY = 0;
       gameHasStarted = false;
@@ -127,13 +112,10 @@ class _FlappyBirdPage extends State<FlappyBirdPage> {
   }
 
   bool birdIsDead() {
-    // check if the bird is hitting the top or the bottom of the screen
     if (birdY < -1 || birdY > 1) {
       return true;
     }
 
-    // hits barriers
-    // checks if bird is within x coordinates and y coordinates of barriers
     for (int i = 0; i < barrierX.length; i++) {
       if (barrierX[i] <= birdWidth &&
           barrierX[i] + barrierWidth >= -birdWidth &&
@@ -151,6 +133,9 @@ class _FlappyBirdPage extends State<FlappyBirdPage> {
     return GestureDetector(
       onTap: gameHasStarted ? jump : startGame,
       child: Scaffold(
+        appBar: AppBar(
+          title: Text('Flappy Bird'),
+        ),
         body: Column(
           children: [
             Expanded(
@@ -214,26 +199,17 @@ class _FlappyBirdPage extends State<FlappyBirdPage> {
                       Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text(
-                            '0',
-                            style: TextStyle(color: Colors.white, fontSize: 35),
-                          ),
+                          
                           SizedBox(
                             height: 15,
                           ),
-                          Text(
-                            'S C O R E',
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                          ),
+                          
                         ],
                       ),
                       Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text(
-                            '10',
-                            style: TextStyle(color: Colors.white, fontSize: 35),
-                          ),
+                          
                           SizedBox(
                             height: 15,
                           ),
